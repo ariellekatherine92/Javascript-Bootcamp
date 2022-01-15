e7d6d2d59e4a2d337f7de2ab8964938122a7ce66
“ariellekatherine92” <ariellekatherine92@gmail.com>
2022-01-15 14:21:07 -0500

    practice

--- lesson1/fundamentals.js
+++ lesson1/fundamentals.js
@@ -1,14 +1,21 @@
 // Exercise 1
 
 
+let superName = "Bruce Wayne";
+let age = 35;
+let home ="Gotham";
 
-
-
+superName="Clark Kent";
+age = 32;
+home ="Metropolis";
 
 
 // Exercise 2
+let firstName = "Han";
+let lastName = "Solo";
+let fullName = `${firstName} ${lastName}`; //string interp
 
-
+console.log(fullName);
 
 
 
@@ -17,11 +24,9 @@
 
 // Exercise 3
 
-
-
-
-
-
+let funAmount = 8;
+funAmount += 2;
+funAmount -=2;
 
 
 
@@ -30,6 +35,10 @@
 // if the "distance" variable is greater than 25
 let distance = 27;
 
+if(distance > 25); {
+console.log("This is far");
+}
+
 
 
 
@@ -37,6 +46,13 @@ let distance = 27;
 // based on the "size" variable value, "S" should log the word "Small"
 let size = 's';
 
+if(size === 's') {
+    console.log('Small')
+} else if (size==='m'){
+    console.log('Medium');
+} else {
+    console.log('Large');
+}
 
 
 
@@ -50,11 +66,11 @@ let size = 's';
 let jerseyNumber = 23;
 let jerseyColor = "yellow";
 
-
-
-
-
+if(jerseyNumber === 23 && jerseyColor === "yellow") {
+    console.log("Lebron James")
+}
 
 // Write an if statement that checks
 // if the variable "someNum" is less than 0 OR greater than 100
 // log stating whether the value meets criteria or not
+
