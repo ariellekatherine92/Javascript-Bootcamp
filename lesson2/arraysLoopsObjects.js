c4b17d0b4e38ffde2b0c0bde933a4e810bdf5fb0
“ariellekatherine92” <ariellekatherine92@gmail.com>
2022-01-15 17:43:40 -0500

    finished course

--- lesson2/arraysLoopsObjects.js
+++ lesson2/arraysLoopsObjects.js
@@ -1,83 +1,83 @@
-// Exercise 1
-// Run this portion of code and walk through the steps of each loop
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+// // Exercise 1
+// // Run this portion of code and walk through the steps of each loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
 
 
-// Now you try, write a for loop that will print out the numbers 1 - 100
+// // Now you try, write a for loop that will print out the numbers 1 - 100
 
 
-for (let i = 0; i <=100; i++) {
-    console.log(i)
-}
-
-
-
-
-// Exercise 2
-// Set a new variable to value of an array
-
-const newVar =[];
-
-
-// Print out the third item in the array
-let breakfast = ['eggs', 'milk', 'cereal', 'orange', 'banana'];
-console.log(breakfast[2])
-
-
-// Set a new variable to the last value of the "breakfast" array
-// Use the .length - 1 method to get the last item of the array
-
-
-for(let i = 0; i<= breakfast.length - 1; i++) {
-    console.log(breakfast[i])
-}
-
-
-// Loop through the array, "breakfast", and log each value within the array
-
-for(let i = 0; i < breakfast.length; i++) {
-    console.log(breakfast[i])
-}
+// for (let i = 0; i <=100; i++) {
+//     console.log(i)
+// }
 
 
 
 
+// // Exercise 2
+// // Set a new variable to value of an array
+
+// const newVar =[];
 
 
-// Exercise 3
-// Set a new variable to a vlue of an object
-let Ariel = {
-    name: "Ariel",
-    age: 29,
-    gender: "female"
-}
-console.log(Ariel.age)
-
-// Print out the "color" value of the "car"
-let car = {
-    color: "blue", 
-    hp: 123,
-    year: 1999
-};
-
-console.log(car.color);
-// Set a new key called "model" on the "car" object, set it with any value
-
-car.model = "Jeep";
+// // Print out the third item in the array
+// let breakfast = ['eggs', 'milk', 'cereal', 'orange', 'banana'];
+// console.log(breakfast[2])
 
 
-// Print out the value you just created
+// // Set a new variable to the last value of the "breakfast" array
+// // Use the .length - 1 method to get the last item of the array
 
-console.log(car.model);
 
-// Loop through the car object with a "for...in loop" and log each key and value
+// for(let i = 0; i<= breakfast.length - 1; i++) {
+//     console.log(breakfast[i])
+// }
 
-for(const key in car) {
-    // console.log('this is the key => ', key);
-    // console.log('this is the value => ',car);
-    console.log(key, ":", car[key]);
-}
+
+// // Loop through the array, "breakfast", and log each value within the array
+
+// for(let i = 0; i < breakfast.length; i++) {
+//     console.log(breakfast[i])
+// }
+
+
+
+
+
+
+// // Exercise 3
+// // Set a new variable to a vlue of an object
+// let Ariel = {
+//     name: "Ariel",
+//     age: 29,
+//     gender: "female"
+// }
+// console.log(Ariel.age)
+
+// // Print out the "color" value of the "car"
+// let car = {
+//     color: "blue", 
+//     hp: 123,
+//     year: 1999
+// };
+
+// console.log(car.color);
+// // Set a new key called "model" on the "car" object, set it with any value
+
+// car.model = "Jeep";
+
+
+// // Print out the value you just created
+
+// console.log(car.model);
+
+// // Loop through the car object with a "for...in loop" and log each key and value
+
+// for(const key in car) {
+//     // console.log('this is the key => ', key);
+//     // console.log('this is the value => ',car);
+//     console.log(key, ":", car[key]);
+// }
 
 
--- lesson3/functions.js
+++ lesson3/functions.js
@@ -1,25 +1,77 @@
 // Exercise 1
 // Write a function declaration
-
+function pickDeclaration() {
+    return pickDeclaration;
+}
 
 // Write a function expression
-
+const ariel = (age, gender) => {
+    return age + gender;
+}
 
 // Write a function using an "arrow" function
 
-
+const subtract = (x, y) => {
+    return x - y;
+}
 
 
 
 // Create a function that will take 2 numbers and log the sum of the 2 numbers
 
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+function sumOfTwo(num1, num2) {
+    return num1 + num2;
+}
+
+// // display the sum
+// console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
 
 
 
 // Create a function that takes in a number and logs from 0 to whatever number that is passed in
 
-
-
+function range(num) {
+    for(let i = 1; i <= num; i++) {
+        console.log(i)
+    }
+    return;
+}
 
 
 // Create a function that takes in a number and logs if it is even or not
+
+const isEven = (num) => {
+    if (num % 2 === 0) {
+      return "is even";
+    } else {
+      return "is odd";
+    }
+  };
+
+
+ //////////////
+
+function sayName(name, age) {
+    return "hi my name is " + name + " and my age is " + age;
+}
+
+const people = [
+    {name: "mary", age: 23}
+
+
+]
+
+
+
+for(let i = 0; i < people.length; i++) {
+    console.log(sayName(people[i].name))
+}
+
+function add (x, y) {
+    return x + y;
+}
+
